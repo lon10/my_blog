41cae5476354c5fd7a9a6f0d628ef94d5a6b2b11
rustam <lon10@rambler.ru>
2014-11-07 10:51:21 +0300

    user.password => BCrypt

---
+++ db/migrate/20141107074835_change_users_column.rb
@@ -0,0 +1,5 @@
+class ChangeUsersColumn < ActiveRecord::Migration
+  def change
+    rename_column :users, :password, :password_hash
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107073032) do
+ActiveRecord::Schema.define(version: 20141107074835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20141107073032) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "password"
+    t.string   "password_hash"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
