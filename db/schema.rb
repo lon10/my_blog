d84e2d7d735802d2eaf3d34558c04121b5c9a7fa
rustam <lon10@rambler.ru>
2014-11-07 10:41:19 +0300

    User model added + admin seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141104085529) do
+ActiveRecord::Schema.define(version: 20141107073032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20141104085529) do
     t.string   "title"
     t.text     "text"
     t.time     "publish_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
