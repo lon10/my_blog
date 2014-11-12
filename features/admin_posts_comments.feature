efaa0eb1c1d315053a34344b121d58c447fb8a4f
rustam <lon10@rambler.ru>
2014-11-12 22:14:27 +0300

    Green tests

--- features/admin_posts_comments.feature
+++ features/admin_posts_comments.feature
@@ -5,15 +5,19 @@ Feature:
   Background:
     Given there is admin
       And there is a post
-      And there is a few comments
       And I signed in as admin
 
+  @javascript
   Scenario: I should be able to see all post comments
-    Given I am on the admin page
+    Given there is a few comments
+      And I am on the admin page
     When I click on edit post comments
-    Then I should see all post
+    Then I should see all post comments
 
+  @javascript
   Scenario: I should be able to delete all post comments
-    Given I am on the existing post comments delete page
-    When I delete post
-    Then post should be deleted
+    Given there is a comment
+      And I am on the existing post comments edit page
+    When I delete comment
+      And I confirm dialog
+    Then comment should be deleted
--- features/step_definitions/admin_posts_comments_steps.rb
+++ features/step_definitions/admin_posts_comments_steps.rb
@@ -1,3 +1,11 @@
+Given(/^there is a comment$/) do
+  post = Post.last
+
+    FactoryGirl.create(:comment,
+                       post: post
+    )
+end
+
 Given(/^there is a few comments$/) do
   post = Post.last
 
@@ -10,6 +18,28 @@ Given(/^there is a few comments$/) do
   end
 end
 
+Then(/^I should see all post comments$/) do
+  page.should have_content('Комментарии к статье')
+
+  comments = Comment.all
+  comments.each do |comment|
+    page.should have_content(comment.name)
+    page.should have_content(comment.comment_preview)
+  end
+end
+
 When(/^I click on edit post comments$/) do
   page.find('.glyphicon-comment').click()
+end
+
+When(/^I delete comment$/) do
+  page.find('.glyphicon-trash').click()
+end
+
+Then(/^comment should be deleted$/) do
+  page.should have_content('Комментарий успешно удалён')
+  page.should have_content('К этой статье пока не добавлено ни одного комментария')
+  page.should_not have_content('some_name')
+  page.should_not have_content('some_text')
+
 end
