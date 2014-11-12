1929e552dca92034b29cb6b0bafade6910c1c4f2
rustam <lon10@rambler.ru>
2014-11-12 23:41:32 +0300

    Post title can not be number

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,15 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
   validates :title, :text, presence: true
+  validate :validate_title
 
   paginates_per(10)
 
   scope :ordered, -> { order('created_at DESC') }
+
+  def should_generate_new_friendly_id?
+    true
+  end
 
   def normalize_friendly_id(input)
     input.to_s.to_slug.normalize(transliterations: :russian).to_s
@@ -17,4 +22,17 @@ class Post < ActiveRecord::Base
   def self.find(id_or_slug)
     self.find_by_id(id_or_slug) || self.find_by_slug(id_or_slug)
   end
+
+  def validate_title
+    if title_is_number?
+      errors.add(:title, 'Название не может быть числом')
+    end
+  end
+
+  private
+
+  def title_is_number?
+    true if Float(title) rescue false
+  end
+
 end
