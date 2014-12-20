d9f63cd2a8f0fdc04f0c8f57744ad7f7d4e97e28
Rustam Manakov <lon10@rambler.ru>
2014-12-20 16:37:20 +0300

    Slice

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module MyBlog
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :ru
 
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.autoload_paths += %W(#{config.root}/lib)
