cb8ca9f5a18a2b89faad5c49b83a99c43551752c
Rustam Manakov <lon10@rambler.ru>
2014-12-26 15:44:24 +0300

    Telegram message on post create

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,15 @@ class Comment < ActiveRecord::Base
 
   validates :name, :text, presence: true
 
+  after_create :send_telegram_message
+
   def comment_preview
     "#{text[0..70]}..."
+  end
+
+  protected
+
+  def send_telegram_message
+    TelegramMessageService.instance.send("#{name} added new comment to <#{post.title}> - #{text}")
   end
 end
---
+++ app/services/telegram_message_service.rb
@@ -0,0 +1,13 @@
+class TelegramMessageService
+  include Singleton
+
+  TG_CONFIG = YAML.load_file(File.expand_path("#{Rails.root}/config/telegram.yml", __FILE__))
+
+  def send(message)
+    if system("#{TG_CONFIG['telegram_path']} -k #{TG_CONFIG['key_path']} -W -e 'msg #{TG_CONFIG['peer']} #{message}'")
+      p 'Telegram sent message'
+    else
+      p 'Error! Telegram obtrivodnomilsya'
+    end
+  end
+end
