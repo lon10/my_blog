b085b73385695b54f17c028e33afa7ba2fa77d24
Rustam Manakov <lon10@rambler.ru>
2014-12-27 15:58:23 +0300

    Remote IP in comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113081449) do
+ActiveRecord::Schema.define(version: 20141227125209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141113081449) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remote_address"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id", using: :btree
