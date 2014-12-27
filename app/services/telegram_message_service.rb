023581b0e22f15c6103e88692fb58c7921b26a78
Rustam Manakov <lon10@rambler.ru>
2014-12-27 14:12:39 +0300

    telegram puts + gitignore

--- app/services/telegram_message_service.rb
+++ app/services/telegram_message_service.rb
@@ -4,10 +4,12 @@ class TelegramMessageService
   TG_CONFIG = YAML.load_file(File.expand_path("#{Rails.root}/config/telegram.yml", __FILE__))
 
   def send(message)
+    p '------------------------'
     if system("#{TG_CONFIG['telegram_path']} -k #{TG_CONFIG['key_path']} -W -e 'msg #{TG_CONFIG['peer']} #{message}'")
       p 'Telegram sent message'
     else
       p 'Error! Telegram obtrivodnomilsya'
     end
+    p '------------------------'
   end
 end
