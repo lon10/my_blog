8d8ea05ecaf0aaddd1aa1eddfa5ebaaace6be0b5
Rustam Manakov <lon10@rambler.ru>
2015-01-29 09:45:11 +0300

    CV

--- app/controllers/cv_controller.rb
+++ app/controllers/cv_controller.rb
@@ -1,13 +1,13 @@
 #curriculum_vitae
 
-class CVController < ApplicationController
+class CvController < ApplicationController
   def en
-    cv = File.join(Rails.root, 'public/curriculum_vitae_en.pdf')
+    cv = File.join(Rails.root, 'public/cv/curriculum_vitae_en.pdf')
     send_file(cv, filename: 'curriculum_vitae_en.pdf', type: 'application/pdf')
   end
 
   def ru
-    cv = File.join(Rails.root, 'public/curriculum_vitae_ru.pdf')
+    cv = File.join(Rails.root, 'public/cv/curriculum_vitae_ru.pdf')
     send_file(cv, filename: 'curriculum_vitae_ru.pdf', type: 'application/pdf')
   end
 end
