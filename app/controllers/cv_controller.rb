c22300e2628e4596c613d483b24b9bf3ef6464e0
Rustam Manakov <lon10@rambler.ru>
2015-01-29 10:22:13 +0300

    Slice

--- app/controllers/cv_controller.rb
+++ app/controllers/cv_controller.rb
@@ -3,11 +3,11 @@
 class CvController < ApplicationController
   def en
     cv = File.join(Rails.root, 'public/cv/curriculum_vitae_en.pdf')
-    send_file(cv, filename: 'curriculum_vitae_en.pdf', type: 'application/pdf')
+    send_file(cv, filename: 'curriculum_vitae_en.pdf', disposition: 'inline', type: 'application/pdf')
   end
 
   def ru
     cv = File.join(Rails.root, 'public/cv/curriculum_vitae_ru.pdf')
-    send_file(cv, filename: 'curriculum_vitae_ru.pdf', type: 'application/pdf')
+    send_file(cv, filename: 'curriculum_vitae_ru.pdf', disposition: 'inline', type: 'application/pdf')
   end
 end
