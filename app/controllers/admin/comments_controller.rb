b9961f535487ecb1fe70049d637ab00187a3b123
Rustam Manakov <lon10@rambler.ru>
2015-10-07 16:05:12 +0300

    Usless methods removed

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -4,18 +4,6 @@ class Admin::CommentsController < Admin::AdminController
     @comments = post.comments
   end
 
-  def destroy
-    comment = Comment.find(params[:id])
-
-    if comment.delete
-      flash[:notice] = t('activerecord.comment.messages.deleted')
-    else
-      flash[:error] = t('errors.messages.main')
-    end
-
-    redirect_to admin_post_comments_path(comment.post, comment)
-  end
-
   def destroy_multiple
     if Comment.destroy(params[:comments])
       flash[:notice] = t('activerecord.comment.messages.multiple_deleted')
